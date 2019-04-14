7a0a65d6759e349e48f2d400baf0390e3182c7f4
lanpn <phan.ngoclan58@gmail.com>
2019-04-14 10:58:43 +0700

    Fix export path

--- scripts/train_5-2.sh
+++ scripts/train_5-2.sh
@@ -2,7 +2,7 @@ python3 keras_retinanet/bin/train.py\
     --backbone resnet50 --batch-size 12\
     --epochs 25 --steps 20000 --lr 1e-4\
     --snapshot-path snapshots/resnet50-LR1e4\
-    --export-path exports/resnet50-LR1e4\
+    --export-path export/resnet50-LR1e4\
     --tensorboard-dir logs/resnet50-LR1e4\
     --random-transform --workers 8\
     --image-min-side 400 --image-max-side 1300\
