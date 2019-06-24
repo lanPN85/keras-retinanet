4a36357ef4321818a86ebff04bf829c8d66a74a6
lanpn <phan.ngoclan58@gmail.com>
2019-06-24 09:31:33 +0700

    Add configs

---
+++ scripts/test_anchors.sh
@@ -0,0 +1,4 @@
+python3 keras_retinanet/bin/debug.py\
+	--image-min-side 600 --image-max-side 1300\
+	--anchors --annotations --config configs/id-config-v8.ini\
+	csv data/crop_5-2/val.csv data/crop_5-2/class_names.csv
