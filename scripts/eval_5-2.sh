d0cde00e33465ea4df205cf24cac58c3fdc97ae2
lanpn <phan.ngoclan58@gmail.com>
2019-07-22 16:27:27 +0700

    Reset config

--- scripts/eval_5-2.sh
+++
@@ -1,7 +0,0 @@
-python3 keras_retinanet/bin/evaluate.py\
-    --backbone resnet50 --score-threshold 0.5\
-    --iou-threshold 0.8 --max-detections 40\
-    --save-path results/resnet50-LR1e4\
-    --image-min-side 400 --image-max-side 1300\
-    csv data/crop_5-2/val.csv data/crop_5-2/class_names.csv\
-    export/resnet50-LR1e4/model.h5
